daeb1d655e3416039892f2d00bbb33da05f44c9f
roobeen-b <rubinbaidhya@gmail.com>
2025-06-17 22:09:16 +0545

    Added command to display summary of the expenses made

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-const { Command } = require("commander");
+#!/usr/bin/env node
+
 const fs = require("fs");
 const path = require("path");
+const { Command } = require("commander");
 
 const program = new Command();
 
 const FILE_PATH = path.join(__dirname, "../expenses.json");
 
-const loadExpenses = () => {
+const loadExpenses = (): TExpense[] => {
   try {
     if (!fs.existsSync(FILE_PATH)) return [];
     const data = fs.readFileSync(FILE_PATH, { encoding: "utf8" });
@@ -69,11 +71,49 @@ program
   .action(() => {
     try {
       const allExpenses = loadExpenses();
+      if (!allExpenses || allExpenses.length <= 0) {
+        console.log("No expense recorded yet.");
+        return;
+      }
       if (allExpenses && allExpenses.length) {
         console.table(allExpenses);
       }
     } catch (error) {
       console.error(`Error while fetching all expenses list: ${error}`);
+    }
+  });
+
+program
+  .command("summary")
+  .description("Provides summary of all the expenses")
+  .option("--month <month>", "Month value in number from 1 to 12")
+  .action((options) => {
+    try {
+      const allExpenses = loadExpenses();
+      const monthValue = Number(options.month);
+      if (monthValue) {
+        if (monthValue > 12 || monthValue < 1) {
+          console.error("Month value must be between 1 and 12 (inclusive)");
+          return;
+        }
+        const expensesByMonth = allExpenses?.filter((item) => {
+          const monthFromDate = item.date.split("-")?.[1];
+          return monthValue === Number(monthFromDate);
+        });
+        const totalExpense = expensesByMonth?.reduce(
+          (sum, item) => sum + item.amount,
+          0
+        );
+        console.log(`Total expenses: $${totalExpense}`);
+        return;
+      }
+      const totalExpense = allExpenses?.reduce(
+        (sum, item) => sum + item.amount,
+        0
+      );
+      console.log(`Total expenses: $${totalExpense}`);
+    } catch (error) {
+      console.error(`Error generating summary: ${error}`);
     }
   });
 
